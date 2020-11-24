62146d86861ddd7888324ed8abdb9a88c0110f08
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-11-24 14:54:44 -0500

    Created RemoteUserStorageProvider class

---
+++ src/main/java/com/appsdeveloperblog/keycloak/RemoteUserStorageProvider.java
@@ -0,0 +1,55 @@
+package com.appsdeveloperblog.keycloak;
+
+import org.keycloak.credential.CredentialInput;
+import org.keycloak.credential.CredentialInputValidator;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.storage.UserStorageProvider;
+import org.keycloak.storage.user.UserLookupProvider;
+
+public class RemoteUserStorageProvider implements UserStorageProvider,
+UserLookupProvider, CredentialInputValidator {
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public UserModel getUserById(String id, RealmModel realm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserModel getUserByUsername(String username, RealmModel realm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserModel getUserByEmail(String email, RealmModel realm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean supportsCredentialType(String credentialType) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isConfiguredFor(RealmModel realm, UserModel user, String credentialType) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isValid(RealmModel realm, UserModel user, CredentialInput credentialInput) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
