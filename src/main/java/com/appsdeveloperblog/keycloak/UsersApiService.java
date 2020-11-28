d141dad26ff3cf7c4874f1e656b400e945aecd08
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-11-28 11:15:22 -0500

    Added the getUserDetails() method to UsersApiService interface

--- src/main/java/com/appsdeveloperblog/keycloak/UsersApiService.java
+++ src/main/java/com/appsdeveloperblog/keycloak/UsersApiService.java
@@ -1,5 +1,20 @@
 package com.appsdeveloperblog.keycloak;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+//@Produces(MediaType.APPLICATION_JSON)
 public interface UsersApiService {
 
+	
+	@GET
+	@Path("/{username}")
+	User getUserDetails(@PathParam("username") String username);
+	
 }
