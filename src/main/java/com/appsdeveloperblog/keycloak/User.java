6d0278dfdb19fbbd53303c133d530b6c034557af
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-11-29 10:26:37 -0500

    Created User class

---
+++ src/main/java/com/appsdeveloperblog/keycloak/User.java
@@ -0,0 +1,42 @@
+package com.appsdeveloperblog.keycloak;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String userName;
+    private String userId;
+    
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+    
+    
+}
