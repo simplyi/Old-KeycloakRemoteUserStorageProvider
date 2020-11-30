95a34feff8b43dc49c6ff49c5c3caa2c120df649
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-11-30 15:08:01 -0500

    Added veryUserPassword() method and VerifyPasswordResponse class

--- src/main/java/com/appsdeveloperblog/keycloak/UsersApiService.java
+++ src/main/java/com/appsdeveloperblog/keycloak/UsersApiService.java
@@ -2,6 +2,7 @@ package com.appsdeveloperblog.keycloak;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,9 +13,13 @@ import javax.ws.rs.core.MediaType;
 //@Produces(MediaType.APPLICATION_JSON)
 public interface UsersApiService {
 
-	
 	@GET
 	@Path("/{username}")
 	User getUserDetails(@PathParam("username") String username);
+	
+    @POST
+    @Path("/{username}/verify-password")
+    VerifyPasswordResponse verifyUserPassword(@PathParam("username") String username, 
+            String password);
 	
 }
---
+++ src/main/java/com/appsdeveloperblog/keycloak/VerifyPasswordResponse.java
@@ -0,0 +1,30 @@
+package com.appsdeveloperblog.keycloak;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author sergeykargopolov
+ */
+public class VerifyPasswordResponse {
+    private boolean result;
+ 
+    public VerifyPasswordResponse(boolean result) {
+		this.result = result;
+	}
+
+	public boolean getResult() {
+        return result;
+    }
+ 
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+ 
+    
+}
